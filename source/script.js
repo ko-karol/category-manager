ce919d5b1c3b27cc283a9a0d8d83c0b9aeffecef
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-12-08 14:08:35 +0000

    elaborated html, steps, and dom initialized in script

--- source/script.js
+++ source/script.js
@@ -1,13 +1,30 @@
-// Your mini-app should take user input for
+// Your mini-app should take user input for a category name, a category icon, and a category colour.
+// Category name should be a string of 4-20 characters.
+// Category icon should be an emoji from the emoji palette below.
+// Category colour should be a colour from the colour palette below.
+// When the user clicks "Add", the category should be added to a list of categories. And added to an object in localStorage.
+// When the user clicks ‘Next’, it should display the recently added category name, icon and colour, with a ‘Back’ button.
+// When the user clicks ‘Back’, the newly created category should now be added back to the list and displayed with the other categories.
+// When the user clicks ‘Delete’, symbolized as the "🗑️" icon,  the category should be removed from the list and localStorage.
+// When the user clicks "Edit", symbolized as the "✏️" icon, the category should be editable using the same input fields as when the category was created.
 
-//     A category name
-//     A category icon
-//     A category colour
+// Color palette: Coral #FF8370, Blue Green #00B1B0, Freesia #FEC84D, Fuschia #E42256, Lilac #BD97CB and Gold #FBC740
+// Emoji palette: 💼 💰 📗 📘 📙 📕
 
-// When the user clicks ‘Next’, it should display the recently added category name, icon and colour, with a ‘Back’ button (not designed).
-// When the user clicks ‘Back’, the newly created category should now be added to the list and displayed.
-// When the user chooses a previously made category and clicks ‘next’, it should display the previously set category name, icon and colour.
-// The user is also able to delete the category by hovering over the category and pressing the ‘delete’ (rubbish bin) icon.
-// The user is able to edit the category by hovering over the category and pressing the ‘edit’ (pencil) icon.
-// Uses localStorage to store the categories.
-// You will be scored on how easy the code is to follow, understand, and maintain. Please give thought to folder structure and any additional unseen elements of your code.
+// <=== DOM ===>
+
+// <-- Elements -->
+const form = document.querySelector(".form");
+const categoryNameInput = document.querySelector("#category-name");
+const iconSelector = document.querySelector("#icon-selector");
+const colorPicker = document.querySelector("#color-picker");
+const addButton = document.querySelector("#add");
+const categoriesList = document.querySelector("#categories");
+
+// <-- Templates -->
+
+const categoryTemplate = document.querySelector("#preset-category");
+const iconTemplate = document.querySelector("#preset-icon");
+const colorTemplate = document.querySelector("#preset-color");
+
+// <=== Functions ===>
