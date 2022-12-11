a1f4095a0e07162ebcead3258266d921036722a0
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-12-11 00:07:08 +0000

    re-added delete functionality and improved readability

--- source/script.js
+++ source/script.js
@@ -15,14 +15,14 @@
 //<-- Elements -->
 
 const form = document.querySelector("#form");
-const categoryNameInput = document.querySelector("#category-name");
+const categoryNameInput = document.querySelector("#form-name-input");
 const iconSelector = document.querySelector("#icon-selector");
 const colorSelector = document.querySelector("#color-picker");
 const categoriesList = document.querySelector("#category-list");
 
 //<-- Variables -->
 
-let categories = [];
+let categoriesArray = [];
 let incrementId = 0;
 
 const palette = {
@@ -48,59 +48,8 @@ const palette = {
 //<=== Helpers ===>
 
 const initialState = (type, value) => type.classList.add(value.toLowerCase());
-
-const categoryTemplate = ({ name, id, icon, color }) => {
-	const template = document.querySelector("#category-template").content.cloneNode(true);
-	const category = template.querySelector("li");
-	const categoryIcon = template.querySelector("span");
-	const categoryName = template.querySelector("h2");
-	const editButton = template.querySelector(".edit");
-	const deleteButton = template.querySelector(".delete");
-
-	category.classList.add(color.toLowerCase());
-	category.id = id;
-	categoryName.textContent = name;
-	categoryIcon.textContent = icon;
-	editButton.textContent = "✏️";
-	deleteButton.textContent = "🗑️";
-
-	deleteButton.addEventListener("click", deleteCategoryHandler);
-	editButton.addEventListener("click", editCategoryHandler);
-
-	return category;
-};
-
-const setCategoryInLocalStorage = (category) => {
-	localStorage.setItem("categories", JSON.stringify(category));
-};
-
-const getCategoriesFromLocalStorage = () => {
-	const storedCategories = JSON.parse(localStorage.getItem("categories"));
-	return storedCategories;
-};
-
-const validateCategoryName = (categoryName) =>
-	categoryName.length >= 4 && categoryName.length <= 20
-		? "isValidCategoryName"
-		: (alert("Category name must be between 4 and 20 characters"), "isNotValidCategoryName");
-
-//<=== Functions ===>
-
-const createNewCategory = (categoryName, icon, color) => {
-	const newCategory = categoryTemplate({ name: categoryName, id: "category-" + incrementId, icon: icon, color: color });
-	categoriesList.append(newCategory);
-
-	incrementId++;
-
-	const categoryInfo = {
-		name: categoryName,
-		id: incrementId,
-		icon: icon,
-		color: color,
-	};
-
-	categories.push(categoryInfo);
-};
+const setCategoryInLocalStorage = (category) => localStorage.setItem("categories", JSON.stringify(category));
+const getCategoriesFromLocalStorage = () => (storedCategories = JSON.parse(localStorage.getItem("categories")));
 
 const populateList = (palette, selector, label) =>
 	palette.map((item) => {
@@ -110,6 +59,46 @@ const populateList = (palette, selector, label) =>
 		option.classList.add(item[label].toLowerCase());
 		selector.appendChild(option);
 	});
+
+const validateCategoryName = (categoryName) =>
+	categoryName.length >= 4 && categoryName.length <= 20
+		? "isValidCategoryName"
+		: (alert("Category name must be between 4 and 20 characters"), "isNotValidCategoryName");
+
+//<=== Functions ===>
+
+const createNewCategory = (categoryName, icon, color) => {
+	const categoryTemplate = ({ name, id, icon, color }) => {
+		const template = document.querySelector("#category-template").content.cloneNode(true);
+
+		const [category, categoryIcon, categoryName, editButton, deleteButton] =
+			template.querySelectorAll("li, span, h2, button, button");
+
+		category.classList.add(color.toLowerCase());
+		category.id = id;
+
+		categoryName.textContent = name;
+		categoryIcon.textContent = icon;
+		editButton.textContent = "✏️";
+		deleteButton.textContent = "🗑️";
+
+		deleteButton.addEventListener("click", deleteCategoryHandler);
+		editButton.addEventListener("click", editCategoryHandler);
+		//category.addEventListener("click", showCategoryHandler);
+
+		return category;
+	};
+
+	const newCategory = categoryTemplate({
+		name: categoryName,
+		id: "category-" + incrementId++,
+		icon: icon,
+		color: color,
+	});
+
+	categoriesList.append(newCategory);
+	categoriesArray.push({ name: categoryName, id: incrementId, icon, color });
+};
 
 //<=== Event Handlers ===>
 
@@ -126,25 +115,39 @@ const nextCategoryHandler = (event) => {
 	const color = colorSelector.value;
 
 	if (categoryName) createNewCategory(categoryName, icon, color);
-	setCategoryInLocalStorage(categories);
+
+	setCategoryInLocalStorage(categoriesArray);
 };
+
+// const showCategoryHandler = (event) => {
+// 	const category = event.target.closest("li");
+// 	const buttons = document.querySelectorAll("button");
+// 	const categories = document.querySelectorAll(`li:not(#${category.getAttribute("id")})`);
+// 	const backButton = document.createElement("button");
+// 	categories.forEach((category) => category.classList.add("hidden"));
+// 	buttons.forEach((button) => button.classList.add("hidden"));
+// 	backButton.textContent = "Back";
+// 	backButton.classList.add("button");
+// 	category.append(backButton);
+// 	category.classList.add("modal");
+// 	backButton.addEventListener("click", backHandler);
+// };
 
 const colorChangeHandler = (event) => {
 	const color = event.target.value;
 	colorSelector.classList.remove(colorSelector.classList[1]);
-	colorSelector.classList.add(color.toLowerCase());
+	iconSelector.classList.remove(iconSelector.classList[1]);
 
-	//for each
+	colorSelector.classList.add(color.toLowerCase());
+	iconSelector.classList.add(color.toLowerCase());
 };
 
 const deleteCategoryHandler = (event) => {
-	//! Broken. Removes the wrong category from localStorage
-	const category = event.target.parentElement;
-	const categoryId = category.getAttribute("id");
-	const categoryIndex = categories.findIndex((category) => category.id === parseInt(categoryId));
-	categories.splice(categoryIndex, 1);
+	const category = event.target.closest("li");
+	const categoryIndex = categoriesArray.findIndex((item) => item.id === category.id);
+	categoriesArray.splice(categoryIndex, 1);
+	setCategoryInLocalStorage(categoriesArray);
 	category.remove();
-	setCategoryInLocalStorage(categories);
 };
 
 const editCategoryHandler = (event) => {
@@ -153,6 +156,15 @@ const editCategoryHandler = (event) => {
 	// Remove the category from the list
 };
 
+const backHandler = (event) => {
+	const category = event.target.closest("li");
+	const categories = document.querySelectorAll(`li:not(#${category.id})`);
+	const buttons = category.querySelectorAll("button");
+	categories.forEach((category) => category.classList.remove("hidden"));
+	buttons.forEach((button) => button.classList.remove("hidden"));
+	category.classList.remove("modal");
+	event.target.remove();
+};
 // <=== Event Listeners ===>
 
 window.addEventListener("load", localStorageHandler);
@@ -161,6 +173,7 @@ colorSelector.addEventListener("change", colorChangeHandler);
 
 // <=== Program ===>
 
+initialState(iconSelector, "Coral");
 initialState(colorSelector, "Coral");
 populateList(palette.icon, iconSelector, "value");
 populateList(palette.color, colorSelector, "name");
