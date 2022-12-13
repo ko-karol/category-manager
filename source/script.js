d37c1fbd3623c31c9a6409ec0e1d05e5c77335fd
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-12-13 23:22:52 +0000

    Added proper input validation

--- source/script.js
+++ source/script.js
@@ -1,63 +1,69 @@
-// <==== Variables ====>
-
 // Select the category list ul element, the form elements and define the color and icon options for categories.
+const form = document.querySelector("#form");
+const formNameInput = document.querySelector("#form-name-input");
+const iconSelector = document.querySelector("#icon-selector");
+const colorSelector = document.querySelector("#color-picker");
 const categoryList = document.querySelector("#category-list");
-const formElements = {
-	form: document.querySelector("#form"),
-	formNameInput: document.querySelector("#form-name-input"),
-	iconSelector: document.querySelector("#icon-selector"),
-	colorPicker: document.querySelector("#color-picker"),
-};
-const palette = {
-	color: [
-		{ name: "Coral", color: "#FF8370" },
-		{ name: "Blue-green", color: "#00B1B0" },
-		{ name: "Freesia", color: "#FEC84D" },
-		{ name: "Fuschia", color: "#E42256" },
-		{ name: "Lilac", color: "#BD97CB" },
-		{ name: "Pinkish", color: "#FCC2D2" },
-	],
 
-	icon: [
-		{ name: "briefcase", value: "💼" },
-		{ name: "moneybag", value: "💰" },
-		{ name: "greenbook", value: "📗" },
-		{ name: "bluebook", value: "📘" },
-		{ name: "orangebook", value: "📙" },
-		{ name: "redbook", value: "📕" },
-	],
-};
+const colorPalette = [
+	{ name: "Coral", color: "#FF8370" },
+	{ name: "Blue-green", color: "#00B1B0" },
+	{ name: "Freesia", color: "#FEC84D" },
+	{ name: "Fuschia", color: "#E42256" },
+	{ name: "Lilac", color: "#BD97CB" },
+	{ name: "Pinkish", color: "#FCC2D2" },
+];
 
-// Define the category array and the id incrementor at which to start the category id.
+const iconPalette = [
+	{ name: "briefcase", value: "💼" },
+	{ name: "moneybag", value: "💰" },
+	{ name: "greenbook", value: "📗" },
+	{ name: "bluebook", value: "📘" },
+	{ name: "orangebook", value: "📙" },
+	{ name: "redbook", value: "📕" },
+];
+
 let categoryArray = [];
-let incrementId = categoryArray.length;
+let incrementId = categoryArray.length || 0;
 
 // <==== Functions ====>
 
-// Set the (category) param to localStorage, only used for categoryArray for now.
+// Helper function to set the category array in local storage
 const setCategoryInLocalStorage = (category) => localStorage.setItem("categories", JSON.stringify(category));
 
-// Create a new category using the category template, add the category to the categoryList and array.
-// Adds the edit and delete event listeners to the buttons generated for each category.
-// The category info is stored in an object and pushed to the categoryArray.
+// Helper to set the initial state of the form selectors
+const initialState = (type, value, color) => {
+	type.value = value;
+	type.classList.add(color.toLowerCase());
+};
+
+// Helper to retrieve the category array from local storage and map each category using the createNewCategory function.
+const localStorageHandler = (event) => {
+	const categories = JSON.parse(localStorage.getItem("categories"));
+	categories.map((category) => createNewCategory(category.name, category.icon, category.color));
+};
+
+// Function to create a new category using the template in generateCategory, then append it to the category list and push it to the category array.
 function createNewCategory(categoryName, icon, color) {
 	const generateCategory = (id, categoryName, icon, color) => {
 		const categoryTemplate = document.querySelector("#category-template").content.cloneNode(true);
 		const newCategory = categoryTemplate.querySelector("li");
-
 		const newCategoryName = categoryTemplate.querySelector("h2");
 		const newCategoryIcon = categoryTemplate.querySelector("span");
 		const newCategoryEdit = categoryTemplate.querySelector(".edit");
 		const newCategoryDelete = categoryTemplate.querySelector(".delete");
 
+		// Set the id, name, icon and edit/delete buttons for the new category.
 		newCategory.id = id;
 		newCategoryName.textContent = categoryName;
 		newCategoryIcon.textContent = icon;
 		newCategoryEdit.textContent = "✏️";
 		newCategoryDelete.textContent = "🗑️";
+
 		newCategoryIcon.classList.add(color.toLowerCase());
 
-		newCategoryEdit.addEventListener("click", editCategory);
+		// Add event listeners to the edit and delete buttons.
+		//TODO: newCategoryEdit.addEventListener("click", editCategory);
 		newCategoryDelete.addEventListener("click", deleteCategory);
 
 		return newCategory;
@@ -65,87 +71,102 @@ function createNewCategory(categoryName, icon, color) {
 
 	const categoryInfo = { id: incrementId, name: categoryName, icon: icon, color: color };
 
+	// Append the new category to the category list and push it to the category array.
 	categoryList.append(generateCategory(incrementId++, categoryName, icon, color));
 	categoryArray.push(categoryInfo);
 }
 
+// When the category is clicked, the category is displayed by itself with next and back buttons. //TODO: Add the ability to display the category's tasks etc.
+//? const showCategory = (event) => {};
 // When the edit button is clicked, the category is editable. //TODO: Add the ability to edit the category name, icon and color.
-const editCategory = (event) => {};
+//? const editCategory = (event) => {};
 
-// When the delete button is clicked, the category is removed from the categoryList and categoryArray then the categoryArray is set to localStorage.
-// categoryIndex is based on the id of the category, which is the index + 1 because the id starts at 1.
-// If the categoryArray is empty, the categories key is removed from localStorage.
+// When the delete button is clicked, the category is deleted from the category list and the category array.
 const deleteCategory = (event) => {
 	const listItem = event.target.closest("li");
 	const categoryIndex = categoryArray.findIndex((category) => category.id == listItem.id);
 	categoryArray.splice(categoryIndex, 1);
+
+	// Reset the incrementId to the length of the category array. This is to ensure that the id's are always unique.
 	incrementId = categoryArray.length + 1;
 	listItem.remove();
 
 	setCategoryInLocalStorage(categoryArray);
 
+	// If there are no categories left, remove the categories key from local storage.
 	if (categoryArray.length == 0) {
 		localStorage.removeItem("categories");
 	}
 };
 
-// Initialize the form elements with the default values and populate the icon and color selectors with the options.
-// If there are categories in localStorage, populate the categoryList with the categories.
-// Validate the category name input and add the category to the categoryList and array when the form is submitted.
-// When the color selector is changed, the color of the icon changes to match the selected color.
-(function initialize() {
-	const { form, formNameInput, iconSelector, colorPicker } = formElements;
+// Helper function to populate the form selectors with the color and icon options and change the color of the icon selector to match the color selected.
+const populateList = (palette, selector, label) => {
+	palette.map((item) => {
+		const option = document.createElement("option");
+		option.value = item[label];
+		option.textContent = item[label];
 
-	const initialState = (type, value, color) => {
-		type.value = value;
-		type.classList.add(color.toLowerCase());
-	};
+		// A bit bugged, there should be no emoji classes... but it works for now.
+		option.classList.add(item[label].toLowerCase());
+		selector.appendChild(option);
+	});
+};
 
-	const localStorageHandler = (event) => {
-		const categories = JSON.parse(localStorage.getItem("categories"));
-		categories.map((category) => createNewCategory(category.name, category.icon, category.color));
-	};
+// Helper function to validate the category name so that it doesn't allow certain categories to be created.
+const validateCategoryName = (categoryName) => {
+	if (categoryName == "") throw new Error("Category name cannot be empty.");
+	if (categoryArray.some((category) => category.name == categoryName)) throw new Error("Category name already exists.");
+	if (categoryName.length > 20) throw new Error("Category name cannot be longer than 20 characters.");
+	if (categoryName.length < 3) throw new Error("Category name cannot be shorter than 3 characters.");
+	if (categoryList.children.length >= 6) throw new Error("You cannot have more than 6 categories.");
+};
 
-	const populateList = (palette, selector, label) =>
-		palette.map((item) => {
-			const option = document.createElement("option");
-			option.value = item[label];
-			option.textContent = item[label];
-			option.classList.add(item[label].toLowerCase());
-			selector.appendChild(option);
-		});
+// Helper function to change the color of the icon selector to match the color selected.
+const colorChangeHandler = (event) => {
+	const color = event.target.value;
+	iconSelector.classList.remove(iconSelector.classList[1]);
+	colorSelector.classList.remove(colorSelector.classList[1]);
+	iconSelector.classList.add(color.toLowerCase());
+	colorSelector.classList.add(color.toLowerCase());
+};
 
-	const addCategory = (event) => {
-		event.preventDefault();
+// Handle the form submission to validate the category name, create a new category and set the category array in local storage.
+const addCategory = (event) => {
+	event.preventDefault();
+	const categoryName = formNameInput.value.trim();
+	const icon = iconSelector.value;
+	const color = colorSelector.value;
 
-		const validateCategoryName = (categoryName) =>
-			categoryName.length >= 4 && categoryName.length <= 20
-				? "isValidCategoryName"
-				: (alert("Category name must be between 4 and 20 characters"), "isNotValidCategoryName");
-
-		const categoryName = formNameInput.value.trim();
-		const icon = iconSelector.value;
-		const color = colorPicker.value;
-
+	// Try to validate the category name, if it fails, display an error message.
+	try {
 		validateCategoryName(categoryName);
 		createNewCategory(categoryName, icon, color);
 		setCategoryInLocalStorage(categoryArray);
-	};
+	} catch (error) {
+		const errorMessage = document.createElement("p");
+		errorMessage.textContent = error.message;
+		formNameInput.classList.add("error");
+		formNameInput.after(errorMessage);
 
-	const colorChangeHandler = (event) => {
-		const color = event.target.value;
-		iconSelector.classList.remove(iconSelector.classList[1]);
-		colorPicker.classList.remove(colorPicker.classList[1]);
-		iconSelector.classList.add(color.toLowerCase());
-		colorPicker.classList.add(color.toLowerCase());
-	};
+		// Remove the error displays after 1 second.
+		setTimeout(() => {
+			formNameInput.classList.remove("error");
+			errorMessage.remove();
+		}, 1000);
+	}
 
-	populateList(palette.color, colorPicker, "name");
-	populateList(palette.icon, iconSelector, "value");
-	initialState(colorPicker, "Coral", "coral");
+	form.reset();
+};
+
+// Use helper functions to start the app with some initializers.
+(function initialize() {
+	populateList(colorPalette, colorSelector, "name");
+	populateList(iconPalette, iconSelector, "value");
+	initialState(colorSelector, "Coral", "coral");
 	initialState(iconSelector, "💼", "coral");
 
 	form.addEventListener("submit", addCategory);
-	colorPicker.addEventListener("change", colorChangeHandler);
+	colorSelector.addEventListener("change", colorChangeHandler);
+
 	localStorage.getItem("categories") ? localStorageHandler() : null;
 })();
